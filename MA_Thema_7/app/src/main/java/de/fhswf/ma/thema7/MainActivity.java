b83ca699fbea9729582f72c549e6907945b7f489
Gin-Wah Chau <ginwah.chau@gmx.de>
2022-01-01 13:45:48 +0100

    Test mult sensors

--- MA_Thema_7/app/src/main/java/de/fhswf/ma/thema7/MainActivity.java
+++ MA_Thema_7/app/src/main/java/de/fhswf/ma/thema7/MainActivity.java
@@ -1,14 +1,67 @@
 package de.fhswf.ma.thema7;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private boolean running = false;
 
+    private SensorManager sensorManager;
+    private Sensor sensor;
+
+    private SensorEventListener gyroscopeSensorListener;
+
+    private TextView xValue;
+    private TextView yValue;
+    private TextView zValue;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        //sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        //sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE_UNCALIBRATED);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        //sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+
+        xValue = (TextView) findViewById(R.id.xValue);
+        yValue = (TextView) findViewById(R.id.yValue);
+        zValue = (TextView) findViewById(R.id.zValue);
+
+        gyroscopeSensorListener = new SensorEventListener()
+        {
+            @Override
+            public void onSensorChanged(SensorEvent event)
+            {
+                //
+                xValue.setText("" + event.values[0]);
+                yValue.setText("" + event.values[1]);
+                zValue.setText("" + event.values[2]);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i)
+            {
+            }
+        };
+
+        // Register the listener
+        sensorManager.registerListener(gyroscopeSensorListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        running = true;
     }
 }
