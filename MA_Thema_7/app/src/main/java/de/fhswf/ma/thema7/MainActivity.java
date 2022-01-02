2d4c7c6eec85db37b05d3e8438cea4e915e9a850
Gin-Wah Chau <ginwah.chau@gmx.de>
2022-01-02 11:19:03 +0100

    Save important Data in Constants Class

--- MA_Thema_7/app/src/main/java/de/fhswf/ma/thema7/MainActivity.java
+++ MA_Thema_7/app/src/main/java/de/fhswf/ma/thema7/MainActivity.java
@@ -10,59 +10,26 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.widget.TextView;
+
+import de.fhswf.ma.thema7.util.Constants;
 
 public class MainActivity extends AppCompatActivity
 {
-    private boolean running = false;
-
-    private SensorManager sensorManager;
-    private Sensor sensor;
-
-    private SensorEventListener gyroscopeSensorListener;
-
-    private TextView xValue;
-    private TextView yValue;
-    private TextView zValue;
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        // getMetrics() Deprecated in API level 30
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        Constants.SCREEN_WIDTH = dm.widthPixels;
+        Constants.SCREEN_HEIGHT = dm.heightPixels;
+        System.out.println("SCREEN_WIDTH " + Constants.SCREEN_WIDTH);
+        System.out.println("SCREEN_HEIGHT " + Constants.SCREEN_HEIGHT);
+
         setContentView(R.layout.activity_main);
-
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        //sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-        //sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE_UNCALIBRATED);
-        //sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
-        //sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR);
-
-        xValue = (TextView) findViewById(R.id.xValue);
-        yValue = (TextView) findViewById(R.id.yValue);
-        zValue = (TextView) findViewById(R.id.zValue);
-
-        gyroscopeSensorListener = new SensorEventListener()
-        {
-            @Override
-            public void onSensorChanged(SensorEvent event)
-            {
-                //
-                xValue.setText("" + event.values[0]);
-                yValue.setText("" + event.values[1]);
-                zValue.setText("" + event.values[2]);
-            }
-
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int i)
-            {
-            }
-        };
-
-        // Register the listener
-        sensorManager.registerListener(gyroscopeSensorListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
-
-        running = true;
     }
 }
---
+++ MA_Thema_7/app/src/main/java/de/fhswf/ma/thema7/util/Constants.java
@@ -0,0 +1,11 @@
+package de.fhswf.ma.thema7.util;
+
+import android.content.Context;
+
+public class Constants
+{
+    public static int SCREEN_WIDTH;
+    public static int SCREEN_HEIGHT;
+
+    public static Context CURRENT_CONTEXT;
+}
