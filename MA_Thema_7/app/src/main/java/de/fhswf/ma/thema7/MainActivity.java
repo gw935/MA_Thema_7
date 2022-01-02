11c8f0fb5b4348cc4808f73d3669bc4a09acfc15
Gin-Wah Chau <ginwah.chau@gmx.de>
2022-01-02 11:32:10 +0100

    Playbutton added (non functional)

--- MA_Thema_7/app/src/main/java/de/fhswf/ma/thema7/MainActivity.java
+++ MA_Thema_7/app/src/main/java/de/fhswf/ma/thema7/MainActivity.java
@@ -11,12 +11,16 @@ import android.hardware.SensorManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import de.fhswf.ma.thema7.util.Constants;
 
 public class MainActivity extends AppCompatActivity
 {
+    private Button playButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -31,5 +35,13 @@ public class MainActivity extends AppCompatActivity
         System.out.println("SCREEN_HEIGHT " + Constants.SCREEN_HEIGHT);
 
         setContentView(R.layout.activity_main);
+
+        playButton = (Button) findViewById(R.id.playButton);
+    }
+
+    // Change to Gameactivity
+    public void playGame(View view)
+    {
+        System.out.println("Play Button pressed");
     }
 }
