1f78fb9a087f77d099b0c9a6bc0b858682adab82
Pascal R <pascal.redmer@gmx.de>
2022-01-02 14:33:46 +0100

    added OrientationData from TYPE_GAME_ROTATION_VECTOR sensor

---
+++ MA_Thema_7/app/src/main/java/de/fhswf/ma/thema7/util/OrientationData.java
@@ -0,0 +1,73 @@
+package de.fhswf.ma.thema7.util;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+/**
+ *
+ * Data from TYPE_GAME_ROTATION_VECTOR
+ *
+ */
+public class OrientationData implements SensorEventListener
+{
+    private SensorManager sensorManager;
+    private Sensor sensor;
+
+    private float[] output = new float[3];
+
+    public OrientationData()
+    {
+        sensorManager = (SensorManager) Constants.CURRENT_CONTEXT.getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR);
+    }
+
+    /**
+     *
+     * Registers a SensorListener for the TYPE_GAME_ROTATION_VECTOR sensor
+     *
+     */
+    public void register()
+    {
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    /**
+     *
+     * unregisters the SensorListener for the TYPE_GAME_ROTATION_VECTOR sensor when the application is not in focus
+     *
+     */
+    public void pause()
+    {
+        sensorManager.unregisterListener(this);
+    }
+
+    /**
+     *
+     * returns sensor output
+     *
+     * @return  returns x,y,z rotation
+     */
+    public float[] getOutput()
+    {
+        return output;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent)
+    {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_GAME_ROTATION_VECTOR)
+        {
+            output = sensorEvent.values;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i)
+    {
+
+    }
+
+}
