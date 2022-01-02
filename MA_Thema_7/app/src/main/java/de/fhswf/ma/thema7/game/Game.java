3946251c06828e71fcc671c5e1f7a28ca8f1131a
Pascal R <pascal.redmer@gmx.de>
2022-01-02 13:29:50 +0100

    GameThread functionality implemented in Game.java

--- MA_Thema_7/app/src/main/java/de/fhswf/ma/thema7/game/Game.java
+++ MA_Thema_7/app/src/main/java/de/fhswf/ma/thema7/game/Game.java
@@ -1,15 +1,19 @@
 package de.fhswf.ma.thema7.game;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import androidx.annotation.MainThread;
 import androidx.annotation.NonNull;
 
 import de.fhswf.ma.thema7.util.Constants;
 
 public class Game extends SurfaceView implements SurfaceHolder.Callback
 {
+    private GameThread thread;
+
     public Game(Context context)
     {
         super(context);
@@ -26,7 +30,13 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback
     @Override
     public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder)
     {
+        // new instance of GameThread
+        thread = new GameThread(getHolder(), this);
+        // used in gameloop
+        Constants.INIT_TIME = System.currentTimeMillis();
 
+        thread.setRunning(true);
+        thread.start();
     }
 
     @Override
@@ -38,11 +48,37 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback
     @Override
     public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder)
     {
-
+        // thread is paused if it isnt in focus
+        boolean retry = true;
+        // while loop because it takes a few attempts
+        while (retry)
+        {
+            try
+            {
+                thread.setRunning(false);
+                thread.join();
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+            retry = false;
+        }
     }
 
+    /**
+     * Gamelogic is updated
+     */
     public void update()
     {
+    }
 
+    /**
+     * Game is rendered
+     *
+     * @param canvas the canvas on which everything is drawn on
+     */
+    public void draw(Canvas canvas)
+    {
+        super.draw(canvas);
     }
 }
